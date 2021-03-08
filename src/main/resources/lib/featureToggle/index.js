45b6e0f413dae08415f28253a38ad29ac2944c3e
Garlov <oe.nordli@gmail.com>
2021-03-08 20:50:25 +0100

    repo and branch setup

--- src/main/resources/lib/featureToggle/index.js
+++ src/main/resources/lib/featureToggle/index.js
@@ -1,3 +1,5 @@
-exports.test = function() {
-  log.info('test')
+const node = require('/lib/featureToggle/node')
+
+exports.create = function(options){
+  log.info(JSON.stringify(options, null, 2))
 }
---
+++ src/main/resources/lib/featureToggle/node.js
@@ -0,0 +1,87 @@
+const nodeLib = require('/lib/xp/node')
+const repoLib = require('/lib/xp/repo')
+const contextLib = require('/lib/xp/context')
+
+const FEATURE_TOGGLE_REPO = 'com.gravitondigital.feature-toggle'
+const FEATURE_TOGGLE_MASTER = 'master'
+const FEATURE_TOGGLE_DRAFT = 'draft'
+
+const getRepo = function() {
+  return repoLib.get(FEATURE_TOGGLE_REPO)
+}
+
+const createRepo = function() {
+  return repoLib.create({
+    id: FEATURE_TOGGLE_REPO,
+    rootPermissions: [
+      {
+        'principal': 'role:admin',
+        'allow': [
+          'READ',
+          'CREATE',
+          'MODIFY',
+          'DELETE',
+          'PUBLISH',
+          'READ_PERMISSIONS',
+          'WRITE_PERMISSIONS'
+        ],
+        'deny': []
+      },
+      {
+        'principal': 'role:feature-toggle.admin',
+        'allow': [
+          'READ',
+          'CREATE',
+          'MODIFY',
+          'DELETE',
+          'PUBLISH',
+          'READ_PERMISSIONS',
+          'WRITE_PERMISSIONS'
+        ],
+        'deny': []
+      },
+      {
+        'principal': 'role:feature.toggle.viewer',
+        'allow': [
+          'READ',
+        ],
+        'deny': [
+          'CREATE',
+          'MODIFY',
+          'DELETE',
+          'PUBLISH',
+          'READ_PERMISSIONS',
+          'WRITE_PERMISSIONS'
+        ]
+      }
+    ]
+  })
+}
+
+const createBranch = function(branch) {
+  return repoLib.createBranch({
+    branchId: branch,
+    repoId: FEATURE_TOGGLE_REPO
+  })
+}
+
+exports.connect = function() {
+  const context = contextLib.get()
+  let repo = getRepo()
+
+  if(!repo) {
+    repo = createRepo()
+  }
+
+  if(repo.branches.filter(b => b === FEATURE_TOGGLE_MASTER).length === 0) {
+    createBranch(FEATURE_TOGGLE_MASTER)
+  }
+  if(repo.branches.filter(b => b === FEATURE_TOGGLE_DRAFT).length === 0) {
+    createBranch(FEATURE_TOGGLE_DRAFT)
+  }
+
+  return nodeLib.connect({
+    repoId: FEATURE_TOGGLE_REPO,
+    branch: context.branch
+  })
+}
